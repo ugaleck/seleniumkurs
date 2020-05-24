bd5f2ff6b593551064346ea2ee793ff22e75e95d
bocharova <galinabocharowa@gmail.com>
2020-05-24 22:05:10 +0200

    Проверка количества любых стикеров у продукта

--- src/test/java/StickersTest.java
+++ src/test/java/StickersTest.java
@@ -22,37 +22,22 @@ public class StickersTest {
     public void start() {
         driver = new FirefoxDriver();
         //driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     @Test
     public void checkStickers() {
         driver.navigate().to("http://localhost/litecart/public_html/en/");
 
-        String mostPopular = "#box-most-popular";
-        checkOnlyOneSticker(mostPopular);
-        String campaigns = "#box-campaigns";
-        checkOnlyOneSticker(campaigns);
-        String latestProducts = "#box-latest-products";
-        checkOnlyOneSticker(latestProducts);
-
-    }
-    //проверяет в каждом элементе списка, найденного по id всего блока, наличие только одногого
-    // стикера, либо New либо Sale
-    public void checkOnlyOneSticker(String listId){
-
-        List<WebElement> products = driver.findElements(By.cssSelector(listId +" li"));
-        System.out.println("popular products" + products.size());
+        List<WebElement> products = driver.findElements(By.cssSelector(".product"));
         for(WebElement product : products){
 
-            int countNewStickers = product.findElements(By.cssSelector("[title=New]")).size();
-            int countSaleStickers = product.findElements(By.cssSelector("[title='On Sale']")).size();
-            int countStickers = countNewStickers + countSaleStickers;
+            int quantityOfStickers = product.findElements(By.cssSelector(".sticker")).size();
 
-            assertEquals(countStickers, 1);
-
+            assertEquals(quantityOfStickers, 1);
         }
+
     }
 
     @After
