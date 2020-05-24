a7b1ca7cad66af4cffcfcf8ff07e31d55814dd03
bocharova <galinabocharowa@gmail.com>
2020-05-24 21:31:48 +0200

    Переделан тест разделов в панели администратора, с циклом

--- src/test/java/AdminSections.java
+++ src/test/java/AdminSections.java
@@ -20,9 +20,9 @@ public class AdminSections {
 
     @BeforeClass
     public static void start() {
-        //driver = new FirefoxDriver();
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver = new FirefoxDriver();
+        //driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
@@ -37,107 +37,29 @@ public class AdminSections {
     }
     @Test
     public void clickAdminSections(){
-        //Appearence
-        driver.findElement(By.xpath("//span[text()='Appearence']")).click();
-        driver.findElement(By.xpath("//h1[text()=' Template']"));
 
-        driver.findElement(By.id("doc-template")).click();
-        driver.findElement(By.xpath("//h1[text()=' Template']"));
-
-        driver.findElement(By.id("doc-logotype")).click();
-        driver.findElement(By.xpath("//h1[text()=' Logotype']"));
-
-        //Catalog
-        String catalog = "//span[text()='Catalog']";
-        driver.findElement(By.xpath(catalog)).click();
-        clickEveryItemFromDirectory(catalog);
-
-        //Countries
-        driver.findElement(By.xpath("//span[text()='Countries']")).click();
-        driver.findElement(By.xpath("//h1"));
-
-        //Currencies
-        driver.findElement(By.xpath("//span[text()='Currencies']")).click();
-        driver.findElement(By.xpath("//h1"));
-
-        //Customers
-        String customers = "//span[text()='Customers']";
-        driver.findElement(By.xpath(customers)).click();
-        driver.findElement(By.xpath("//h1"));
-        clickEveryItemFromDirectory(customers);
-
-        //Geo Zones
-        driver.findElement(By.xpath("//span[text()='Geo Zones']")).click();
-        driver.findElement(By.xpath("//h1"));
-
-        //Languages
-        String languages = "//span[text()='Languages']";
-        driver.findElement(By.xpath(languages)).click();
-        driver.findElement(By.xpath("//h1"));
-        clickEveryItemFromDirectory(languages);
-
-        //Modules
-        String modules = "//span[text()='Modules']";
-        driver.findElement(By.xpath(modules)).click();
-        driver.findElement(By.xpath("//h1"));
-        clickEveryItemFromDirectory(modules);
-
-        //Orders
-        String orders = "//span[text()='Orders']";
-        driver.findElement(By.xpath(orders)).click();
-        driver.findElement(By.xpath("//h1"));
-        clickEveryItemFromDirectory(orders);
-
-        //Pages
-        driver.findElement(By.xpath("//span[text()='Pages']")).click();
-        driver.findElement(By.xpath("//h1"));
-
-        //Reports
-        String reports = "//span[text()='Reports']";
-        driver.findElement(By.xpath(reports)).click();
-        driver.findElement(By.xpath("//h1"));
-        clickEveryItemFromDirectory(reports);
-
-        //Settings
-        String settings = "//span[text()='Settings']";
-        driver.findElement(By.xpath(settings)).click();
-        driver.findElement(By.xpath("//h1"));
-        clickEveryItemFromDirectory(settings);
-
-        //Slides
-        driver.findElement(By.xpath("//span[text()='Slides']")).click();
-        driver.findElement(By.xpath("//h1"));
-
-        //Tax
-        String tax = "//span[text()='Tax']";
-        driver.findElement(By.xpath(tax)).click();
-        driver.findElement(By.xpath("//h1"));
-        clickEveryItemFromDirectory(tax);
-
-        //Translations
-        String translations = "//span[text()='Translations']";
-        driver.findElement(By.xpath(translations)).click();
-        driver.findElement(By.xpath("//h1"));
-        clickEveryItemFromDirectory(translations);
-
-        //Users
-        driver.findElement(By.xpath("//span[text()='Users']")).click();
-        driver.findElement(By.xpath("//h1"));
-
-        //vQmods
-        String vQmods = "//span[text()='vQmods']";
-        driver.findElement(By.xpath(translations)).click();
-        driver.findElement(By.xpath("//h1"));
-        clickEveryItemFromDirectory(vQmods);
-
-    }
-    public void clickEveryItemFromDirectory(String directory){
-        List<WebElement> directoryItems = driver.findElements(By.xpath
-                (directory+"/../../ul[@class='docs']/li"));
-
-        for(int i = 1; i<=directoryItems.size(); i++){
-            driver.findElement(By.xpath(directory+"/../../ul[@class='docs']/li["+i+"]")).click();
+        int quantityOfMenuTabs = driver.findElements(By.cssSelector("ul#box-apps-menu li")).size();
+        System.out.println(quantityOfMenuTabs);
+        for(int i = 1; i<=quantityOfMenuTabs; i++){
+            String actualTabXpath = "//ul[@id='box-apps-menu']/li["+i+"]";
+            driver.findElement(By.xpath(actualTabXpath)).click();
             driver.findElement(By.xpath("//h1"));
+
+            int quantityOfSubTabs = driver.findElements(By.xpath(actualTabXpath+"//li")).size();
+            System.out.println("Subtabs: " + quantityOfSubTabs);
+
+            if(quantityOfSubTabs>0){
+                for(int j=1; j<=quantityOfSubTabs; j++){
+                    String actualSubTabXpath = actualTabXpath+"//li"+"["+j+"]";
+
+                    WebElement actualSubTab = driver.findElement(By.xpath(actualSubTabXpath));
+                    System.out.println("actual subtab "+actualSubTab);
+
+                    actualSubTab.click();
+                    driver.findElement(By.xpath("//h1"));
+                }
+            }
+
         }
     }
 
